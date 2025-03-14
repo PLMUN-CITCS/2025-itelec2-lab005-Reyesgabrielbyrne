f2ee846411085cc6ca6fce52e2096aea74180e1e
Gabriellllll Reyes <reyesgabrielbyrne_bsit@plmun.edu.ph>
2025-03-14 20:41:03 +0800

    Submitting Python Week 02 - Session 01 - Exercise 03

---
+++ exercise_03.py
@@ -0,0 +1,31 @@
+a = 10
+b = 5
+c = 3
+
+
+result1 = a + b * c  # Multiplication before addition
+print("Result of a + b * c:", result1)
+
+
+result2 = (a + b) * c  # Addition within parentheses first
+print("Result of (a + b) * c:", result2)
+
+
+result3 = a - b
+print("Result of a - b:", result3)
+
+# Use standard and floor division
+result4 = a / b  # Standard division
+result5 = a // b  # Floor division
+print("Result of a / b:", result4)
+print("Result of a // b (floor division):", result5)
+
+
+result6 = a % b  # Modulus (remainder of division)
+result7 = a ** c  # Exponentiation
+print("Result of a % b (modulus):", result6)
+print("Result of a ** c (exponentiation):", result7)
+
+
+result8 = (a + b - c) * (a / b)
+print("Result of (a + b - c) * (a / b):", result8)
